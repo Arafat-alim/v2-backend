ac3a7c68cbfc29f342d98c8610bc1d4c171e7e9a
ARAFAT AMAN ALIM 🇮🇳 <76907565+Arafat-alim@users.noreply.github.com>
2025-07-08 00:43:12 +0530

    🐞 Bugs Fixed: Post VIsitor and Submit Contact form issue has been resolved.

--- src/routes/contact.routes.js
+++ src/routes/contact.routes.js
@@ -1,7 +1,5 @@
 import { Router } from 'express';
 import { submitContact } from '../controller/contact.controller.js';
-import { validate } from '../middleware/validate.js';
-import { contactSchema } from '../validators/contact.validator.js';
 
 const contactRouter = Router();
 
